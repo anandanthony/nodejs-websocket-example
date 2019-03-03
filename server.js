e982d6157265a387f5b3ffebb4ed7e41cd63b24e
anandanthony <anfranci@microsoft.com>
2019-03-03 22:03:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var port = process.env.PORT || 3000;
 
 var app = express()
 
+var server = require('http').createServer(app);
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/ws.html');
 })
 
-app.listen(port, function () {
+server.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
